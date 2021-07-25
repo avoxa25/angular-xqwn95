2881cf42b08d8dc843528ecf6fa61fcce64b498d
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-07-25 23:34:56 +0300

    Add notify

--- src/app/product-alert/product-alert.component.ts
+++ src/app/product-alert/product-alert.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Input } from '@angular/core';
+import { Product } from '../products';
 @Component({
   selector: 'app-product-alert',
   templateUrl: './product-alert.component.html',
   styleUrls: ['./product-alert.component.css']
 })
 export class ProductAlertComponent implements OnInit {
-
+  @Input() product!: Product;
   constructor() { }
 
   ngOnInit() {
